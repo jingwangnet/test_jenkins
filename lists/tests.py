8a9e1ed115cc807e471b551756b28a9c38f81836
jing wang <admin@jingwangnet.com>
2022-10-23 16:52:42 +0800

    modified:   lists/tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 # Create your tests here.
 class SmokeTest(TestCase):
     def test_bad_math(self):
-        self.assertEqual(1 + 1, 2)
+        self.assertEqual(1 + 1, 3)
